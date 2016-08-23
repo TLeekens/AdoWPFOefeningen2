9b98d55aaafecdcfd06303daa1e0ed028db1c885
TLeekens <tony.leekens@skynet.be>
2016-08-23 15:58:31 +0200

    Bezig met validatie, VerkoopPrijsRangeRule moet nog testen op leeg (morgen)

---
+++ AdoWPFOefeningen2/VerkoopPrijsRangeRule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Globalization;
+
+namespace AdoWPFOefeningen2
+{
+    public class VerkoopPrijsRangeRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            Decimal verkoopPrijs = 0;
+            NumberStyles style = NumberStyles.Currency;
+
+            if (!Decimal.TryParse(value.ToString(), style, cultureInfo, out verkoopPrijs))
+            {
+                return new ValidationResult(false, "Prijs moet een getal zijn");
+            }
+
+            if (verkoopPrijs < 0)
+            {
+                return new ValidationResult(false, "Prijs mag niet negatief zijn");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+    }
+}
